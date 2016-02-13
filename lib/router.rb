13be6efe3a25ec6fe523c9c59cb2b965dd0cf107
Thomas Hopkins <thomas.john.hopkins@gmail.com>
2016-02-13 13:32:37 -0500

    Add route helpers. Begin to merge Active Record functionality.

--- lib/router.rb
+++ lib/router.rb
@@ -1,3 +1,5 @@
+require_relative './route_helpers'
+
 class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
@@ -6,6 +8,7 @@ class Route
     @http_method = http_method
     @controller_class = controller_class
     @action_name = action_name
+    add_route_helpers
   end
 
   # check if route params match the request params
@@ -25,6 +28,51 @@ class Route
     @controller_class
       .new(request, response, route_params)
       .invoke_action(action_name)
+  end
+
+  private
+
+    def add_route_helpers
+      case action_name
+      when :edit
+        name = "edit_#{ class_name_singular }"
+        add_path_method(name, "/#{ class_name_plural }/:id/edit")
+      when :new
+        name = "new_#{ class_name_singular }"
+        add_path_method(name, "/#{ class_name_plural }/new")
+      when :show, :destroy, :update
+        name = "#{ class_name_singular }"
+        add_path_method(name, "/#{ class_name_plural }/:id")
+      when :index, :create
+        name = "#{ class_name_plural }"
+        add_path_method(name, "/#{ name }")
+      end
+    end
+
+    def class_name
+      controller_class.to_s.underscore.gsub('_controller', '')
+    end
+
+    def class_name_plural
+      class_name.pluralize
+    end
+
+    def class_name_singular
+      class_name.singularize
+    end
+
+    def add_path_method(name, path)
+      path_name = "#{ name }_path"
+      puts "#{ path_name } #=> #{ path }"
+
+      RouteHelpers.send(:define_method, path_name) do |*args|
+        id = args.first.to_s
+        if path.include?(':id') && !id.nil?
+          path.gsub!(':id', id)
+        end
+        path
+      end
+    end
   end
 end
 

02ba89f302afa7991f647f8ade0e02af8e2af1c6
Thomas Hopkins <thomas.john.hopkins@gmail.com>
2016-02-13 17:06:16 -0500

    Edit sql_object, connection, and searchable files in Active Record.

---
+++ active_record/searchable.rb
@@ -0,0 +1,16 @@
+module Searchable
+  def where(params)
+    where_details = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_details}
+    SQL
+
+    parse_all(results)
+  end
+end

811af0a60ef55e2c28f66c90d3a533784dfe476a
Thomas Hopkins <thomas.john.hopkins@gmail.com>
2016-02-13 13:12:40 -0500

    Add methods to Router class. Also added a Flash file with a Flash class.

--- lib/router.rb
+++ lib/router.rb
@@ -27,3 +27,48 @@ class Route
       .invoke_action(action_name)
   end
 end
+
+
+class Router
+  attr_reader :routes
+
+  def initialize
+    @routes = []
+  end
+
+  def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(
+      pattern,
+      method,
+      controller_class,
+      action_name
+    )
+  end
+
+  # syntactic sugar to evaluate the proc in the context of the instance
+  def draw(&proc)
+    instance_eval(&proc)
+  end
+
+  # all possible routes
+  [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
+  end
+
+  def match(request)
+    routes.find { |route| route.matches?(request) }
+  end
+
+  # throw 404 if a route match isn't found
+  def run(request, response)
+    matching_route = match(request)
+
+    if matching_route.nil?
+      response.status = 404
+    else
+      matching_route.run(request, response)
+    end
+  end
+end
